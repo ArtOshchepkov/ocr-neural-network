aae616ff1caca23b4f2e1b4dab6f10c93141a10d
aoshchepkov <semitro_8@mail.ru>
2022-01-12 12:33:36 +0700

    fix some defects

--- network.py
+++ network.py
@@ -1,18 +1,23 @@
 import sys
 from random import random
 from typing import List
+import matplotlib.pyplot as plt
 
 import numpy as np
 
 input = np.zeros((16, 16))
 
-speed = 0.01
+learn_speed = 0.00001
+
+
+class LearningLog:
+    loss = []
 
 
 class Network:
-    EPOCHS = 200
+    EPOCHS = 300
 
-    forget_speed = 10
+    forget_speed = 15
 
     loss_value = 0
 
@@ -22,6 +27,7 @@ class Network:
         self.layers = [
             Dense(16 * 16, 27),
         ]
+        self.learning_log = LearningLog()
 
     def predict(self, x):
         for layer in self.layers:
@@ -37,14 +43,22 @@ class Network:
 
         for i in range(0, self.EPOCHS):
             self.loss_value = self.total_loss(xs, ys)
-            print(f'Epoch {i}, loss {self.loss_value}', flush=True)
+            self.learning_log.loss.append(np.sum(self.loss_value))
+            print(f'Epoch {i}, loss {np.sum(self.loss_value)} {self.loss_value}', flush=True)
             for x, y in sorted(zip(xs, ys), key=lambda _: random()):
                 self.teach_sample(x, y)
+
+    def printLog(self):
+        plt.figure()
+        print(self.learning_log.loss)
+        plt.plot(self.learning_log.loss[10:])
+        plt.show()
 
     def total_loss(self, xs: List[np.array], ys: List[np.array]):
         error = 0
         for x, y in zip(xs, ys):
-            error += self.loss(x, y)
+            pred = self.predict(x)
+            error += self.loss(pred, y)
         return error / len(xs)
 
     def teach_sample(self, x: np.array, y: np.array):
@@ -52,18 +66,18 @@ class Network:
         SGD
         """
         pred = self.predict(x)
-        print(f'Prediceed shape {pred.shape}')
-        err = self.loss_derivative(pred, y)
+        # print(f'Predict shape {pred.shape}')
+        err = self.loss(pred, y)
         self.loss_value = self.forget_speed * err + (1 - self.forget_speed) * self.loss_value
-        local_grad = err
+        local_grad = self.loss_derivative(pred, y)
         for layer in reversed(self.layers):
             local_grad = layer.teach(local_grad)
 
-    def loss(self, x: np.array, y: np.array):
-        return (self.predict(x) - y) ** 2
+    def loss(self, pred: np.array, y: np.array):
+        return 0.5 * (pred - y) ** 2
 
     def loss_derivative(self, prediction, y):
-        return 2 * (prediction - y)
+        return prediction - y
 
 
 def relu(x):
@@ -71,7 +85,7 @@ def relu(x):
 
 
 def relu_derivative(x):
-    return x if x > 0 else 0
+    return x if x > 1 else 0
 
 
 class Dense:
@@ -80,17 +94,20 @@ class Dense:
     def __init__(self, in_shape, out_shape) -> None:
         self.in_shape = in_shape
         self.out_shape = out_shape
-        self.w = np.empty((out_shape, in_shape))
+        self.w = np.random.rand(out_shape, in_shape) - 0.5
+        # print(self.w)
         print('Init Dense layer with shape ' + str(self.w.shape))
 
     def f(self, x: np.array) -> np.array:
         signal = self.w.dot(x)
         activation = np.vectorize(relu)
-        return np.transpose(activation(signal))
+        return activation(signal)
 
     def teach(self, local_grad: np.array) -> np.array:
         activation_grad_f = np.vectorize(relu_derivative)
         activation_grad = activation_grad_f(local_grad)
-        w_grad = activation_grad * self.w
-        self.w -= speed * w_grad
+        w_grad = activation_grad * self.w.transpose()  # TODO: transpose correct?
+        self.w -= (learn_speed * w_grad).transpose()  # TODO: transpose correct?
+        # print(self.w)
+        # print(f'Input/Output local grads shapes: {local_grad.shape} {w_grad.shape}')
         return w_grad
