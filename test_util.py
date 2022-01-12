760d1bf7fdc6bdd43c0f28e7af2c610c75d6cebb
aoshchepkov <semitro_8@mail.ru>
2022-01-12 13:34:02 +0700

    add util test

---
+++ test_util.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+from main import y_to_letter
+
+
+def test_y_to_char():
+    y = np.zeros(26)
+    y[0] = 1.0
+
+    char = y_to_letter(y)
+
+    assert char == 'A'
+
+
+def test_y_to_char_z():
+    y = np.zeros(26)
+    y[25] = 1.0
+
+    char = y_to_letter(y)
+
+    assert char == 'Z'
